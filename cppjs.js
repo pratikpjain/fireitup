2986359c1a4c78171236a569036dc441c5641f3d
pratikpjain <63181338+pratikpjain@users.noreply.github.com>
2020-06-24 11:04:27 +0530

    Update cppjs.js

--- cppjs.js
+++ cppjs.js
@@ -1,6 +1,8 @@
 var questions = ['Which of the following is a valid in-built data-type in C++ language ?','Which of the following is NOT the access specifier in c++ ?','Scope of private data members is : ','Constructor in classes are used for : ','What is the output of : cout<<(char)51;  : '];
 var options = [['list', 'string' , 'tuple' , 'dict'],['public', 'private' , 'protected' , 'friend'],['for the child classes', 'for whole program' , 'for main() function only' , 'for friend functions'],['returning values of data members', 'getters and setters' , 'initializing the data members' , 'All of these'],['Error', '2' , '3' , '4']];
 var corr_opt = [['op2','que0','string'] , ['op8','que1','friend'] , ['op12','que2','for friend functions'] , ['op15','que3','initializing the data members'] , ['op19','que4','3']];
+var sec;
+var int;
 function startquiz() {
     document.querySelector(".start").style.display = "none";
     var data = document.querySelector("section");
@@ -15,8 +17,13 @@ function startquiz() {
         data.innerHTML += '<div class = "list" >' + '<h3 class = "que" >' + 'Q.' + (i+1) + ' ' + questions[i] + '</h3>' + '<ul id = que' + i + '>' + x + '</ul>' + '</div>';
     }
     document.querySelector('section').innerHTML += '<div class = "submit"><button class = "btn" onclick = "submitquiz()">submit</button></div>';
+    sec = 60;
+    int = setInterval(timer, 1000)
 }
 function submitquiz() {
+    window.clearInterval(int);
+    document.querySelector('.time').className = 'time';
+    document.querySelector('.time').innerHTML = "Timer";
     var marks = 0;
     var count = 1;
     for(var k = 0 ; k < 5 ; k++){
@@ -38,4 +45,29 @@ function submitquiz() {
     document.querySelector('.submit').style.display = 'none';
     document.querySelector('section').innerHTML += '<div class = "score"><button class = "btn scr">Score = ' + marks + '/100</button></div>';
     document.querySelector('section').innerHTML += '<div class = "restart"><button class = "btn" onclick = "startquiz()">Retake</button></div>';
+}
+function timer() {
+    sec--;
+    var temp = document.querySelector('.time');
+    if (typeof (temp) != undefined && temp != null) { }
+    else
+        document.querySelector('body').innerHTML += '<button class = "time"></button>';
+    if (sec >= 0) {
+        if (sec >= 10) {
+            document.querySelector('.time').className = 'time';
+            document.querySelector('.time').innerHTML = "Time : " + sec;
+        }
+        else {
+            document.querySelector('.time').className = 'time red';
+            document.querySelector('.time').innerHTML = "Time : 0" + sec;
+        }
+    }
+    else {
+        window.clearInterval(int);
+        document.querySelector('audio').play();
+        document.querySelector('.time').innerHTML = "Time : 00";
+        window.confirm("Sorry!! TimeUp !!\n Click 'ok' to continue");
+        submitquiz();
+    }
+    return;
 }
