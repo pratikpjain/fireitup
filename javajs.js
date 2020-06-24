82a3688b649fd7b1f0d73a429ef45c9386aab055
pratikpjain <63181338+pratikpjain@users.noreply.github.com>
2020-06-24 11:06:51 +0530

    Update javajs.js

--- javajs.js
+++ javajs.js
@@ -1,6 +1,8 @@
 var questions = ['Which of the following individuals is credited for first designing java ?','Within Java , each call to StdOut.println() does which of the following ?','Which company now owns Java ?','Which keyword is used to skip the current iteration in loop ?','Which keyword keeps the main method from returning any value to the caller ?'];
 var options = [['James Gosling', 'Jim LeValley' , 'Ian Sheeler' , 'Tim Ritchey'],['Reads and then prints the contents of the current file', 'Prints the contents of the current file' , 'Appends text to the end of the file' , 'Overwrites the current contents of the file'],['PeopleSoft Inc.', 'Oracle Corporation' , 'SAP' , 'Apache Software Foundation'],['break;', 'skip;' , 'pass;' , 'continue;'],['exclusive', 'restricted' , 'void' , 'concealed']];
 var corr_opt = [['op1','que0','James Gosling'] , ['op7','que1','Appends text to the end of the file'] , ['op10','que2','Oracle Corporation'] , ['op16','que3','continue;'] , ['op19','que4','void']];
+var sec;
+var int;
 function startquiz() {
     document.querySelector(".start").style.display = "none";
     var data = document.querySelector("section");
@@ -15,8 +17,13 @@ function startquiz() {
         data.innerHTML += '<div class = "list" >' + '<h3 class = "que" >' + 'Q.' + (i+1) + ' ' + questions[i] + '</h3>' + '<ul id = que' + i + '>' + x + '</ul>' + '</div>';
     }
     document.querySelector('section').innerHTML += '<div class = "submit"><button class = "btn" onclick = "submitquiz()">submit</button></div>';
+    sec = 60;
+    int = setInterval(timer, 1000);
 }
 function submitquiz() {
+    window.clearInterval(int);
+    document.querySelector('.time').className = 'time';
+    document.querySelector('.time').innerHTML = "Timer";
     var marks = 0;
     var count = 1;
     for(var k = 0 ; k < 5 ; k++){
@@ -38,4 +45,29 @@ function submitquiz() {
     document.querySelector('.submit').style.display = 'none';
     document.querySelector('section').innerHTML += '<div class = "score"><button class = "btn scr">Score = ' + marks + '/100</button></div>';
     document.querySelector('section').innerHTML += '<div class = "restart"><button class = "btn" onclick = "startquiz()">Retake</button></div>';
+}
+function timer() {
+    sec--;
+    var temp = document.querySelector('.time');
+    if (typeof (temp) != undefined && temp != null) { }
+    else
+        document.querySelector('body').innerHTML += '<button class = "time"></button>';
+    if (sec >= 0) {
+        if (sec >= 10) {
+            document.querySelector('.time').className = 'time';
+            document.querySelector('.time').innerHTML = "Time : " + sec;
+        }
+        else {
+            document.querySelector('.time').className = 'time red';
+            document.querySelector('.time').innerHTML = "Time : 0" + sec;
+        }
+    }
+    else {
+        window.clearInterval(int);
+        document.querySelector('audio').play();
+        document.querySelector('.time').innerHTML = "Time : 00";
+        window.confirm("Sorry!! TimeUp !!\n Click 'ok' to continue");
+        submitquiz();
+    }
+    return;
 }
